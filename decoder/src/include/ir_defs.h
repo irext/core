5f8200f4dc83073ecea5e125482977ded9af906c
strawmanbobi <strawmanbobi@163.com>
2026-01-27 19:54:38 +0800

    enabled debug

--- decoder/src/include/ir_defs.h
+++ decoder/src/include/ir_defs.h
@@ -14,6 +14,8 @@ Revision log:
 
 #define IR_DECODE_LIB_VER "1.5.2"
 
+#define DEBUG  (1)
+
 #if defined (BOARD_PC)
 #pragma ide diagnostic ignored "OCUnusedGlobalDeclarationInspection"
 #pragma ide diagnostic ignored "OCUnusedMacroInspection"
